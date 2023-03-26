94b1b75907113fd03c2c2ce4c1f6044b14a96905
ted-thuraa <mutwirited@gmail.com>
2023-03-26 18:17:32 +0300

    few updates

--- src/components/AdminLayout.jsx
+++ src/components/AdminLayout.jsx
@@ -25,6 +25,7 @@ export default function AdminLayout() {
   const [isAdmin, setIsAdmin] = useState(false);
   const [adminLoaded, setAdminLoaded] = useState(false);
   const [email, setEmail] = useState();
+  const [sidebarOpen, setSidebarOpen] = useState(false);
   const { user, token, setUser, setToken } = useStateContext();
   const {
     currentColor,
@@ -69,6 +70,10 @@ export default function AdminLayout() {
     }
   }, []);
 
+  const _setSidebarOpen = (ev) => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
   const onLogout = (ev) => {
     ev.preventDefault();
 
@@ -93,28 +98,63 @@ export default function AdminLayout() {
             <div class="px-3 py-3 lg:px-5 lg:pl-3">
               <div class="flex items-center justify-between">
                 <div class="flex items-center justify-start">
-                  <button
-                    data-drawer-target="logo-sidebar"
-                    data-drawer-toggle="logo-sidebar"
-                    aria-controls="logo-sidebar"
-                    type="button"
-                    class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                  >
-                    <span class="sr-only">Open sidebar</span>
-                    <svg
-                      class="w-6 h-6"
-                      aria-hidden="true"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
+                  {sidebarOpen && (
+                    <button
+                      onClick={(ev) => {
+                        _setSidebarOpen(ev);
+                      }}
+                      data-drawer-target="logo-sidebar"
+                      data-drawer-toggle="logo-sidebar"
+                      aria-controls="logo-sidebar"
+                      type="button"
+                      class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                     >
-                      <path
-                        clip-rule="evenodd"
-                        fill-rule="evenodd"
-                        d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
-                      ></path>
-                    </svg>
-                  </button>
+                      <span class="sr-only">Open sidebar</span>
+                      <svg
+                        class="w-6 h-6"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          clip-rule="evenodd"
+                          fill-rule="evenodd"
+                          d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+                        ></path>
+                      </svg>
+                    </button>
+                  )}
+
+                  {!sidebarOpen && (
+                    <button
+                      onClick={(ev) => {
+                        _setSidebarOpen(ev);
+                      }}
+                      data-drawer-target="logo-sidebar"
+                      data-drawer-toggle="logo-sidebar"
+                      aria-controls="logo-sidebar"
+                      type="button"
+                      class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                    >
+                      <span class="sr-only">Open sidebar</span>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-6 h-6"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M6 18L18 6M6 6l12 12"
+                        />
+                      </svg>
+                    </button>
+                  )}
+
                   <Link to="/" className="-m-1.5 p-1.5 flex items-center">
                     <img className="h-10 w-10 logo_rgb" src={logo} alt="logo" />
                     <span className="text-slate-300 logo_rgb sm:text-3xl text-2xl font-bold ml-2 tracking-tight">
@@ -191,7 +231,9 @@ export default function AdminLayout() {
 
           <aside
             id="logo-sidebar"
-            class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
+            class={`${
+              sidebarOpen ? "-translate-x-full" : "translate-x-0"
+            } fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700`}
             aria-label="Sidebar"
           >
             <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
